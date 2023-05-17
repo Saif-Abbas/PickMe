a2ca5c7482744dc6a4c82f5457c96903aec4d874
i3amber <saoab1@hotmail.com>
2023-05-18 00:39:41 +0300

    Creating the Nav Menu

--- src/components/CustomDrawerContent.js
+++ src/components/CustomDrawerContent.js
@@ -1,47 +1,146 @@
-import * as React from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import Constants from 'expo-constants';
-import { colors, device, fonts } from '../constants';
-import NavButton from './NavButton';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import NavProfileHeader from './NavProfileHeader';
+import { gStyle } from '../constants';
 
-const CustomDrawerContent = () => (
-  <View style={styles.container}>
-    {/* Navigation here  */}
-    {/* HEREeee YOU PUT AAAAA ANOTHER COMPONENT CALLED NAVPROFILEHEADER MAYBE */}
-    {/* HERE PLEASE PUT A TERNARY EXPRESSION TO CHECK IF USER IS LOGGED IN OR NOT */}
-    {/* IF USER IS LOGGED IN THEN SHOW THE PROFILE HEADER & THE BUTTONS */}
-    {/* FOR EXAMPLE:  */}
-    {/* {user ? <NavProfileHeader /> : <NavButton name="Login" action={() => Alert.alert('hi')} />} */}
-    <NavProfileHeader
-      img={require('../assets/icon.png')}
-      action={() => Alert.alert('Profile Img')}
-    />
-    <NavButton name="Home" action={() => Alert.alert('hi')} />
-    <NavButton name="Profile" action={() => Alert.alert('profile')} />
-    <View style={styles.containerVersion}>
-      <Text style={styles.versionText}>{`v${Constants.manifest.version}`}</Text>
+const CustomwerDrawerContent = () => {
+  const userInfo = {
+    img: 'user_avatar.jpg',
+    name: 'Saif Abbas',
+    rating: 4.5
+  };
+
+  const handleTripHistory = () => {
+    // Handle Trip History navigation
+  };
+
+  const handleBecomeDriver = () => {
+    // Handle Become Driver navigation
+  };
+
+  const handlePaymentMethod = () => {
+    // Handle Payment Methods navigation
+  };
+
+  const handleAbout = () => {
+    // Handle About navigation
+  };
+
+  const handleHelp = () => {
+    // Handle Help navigation
+  };
+
+  const handleSignInOut = () => {
+    // Handle Sign In/Out
+  };
+
+  const handleSettings = () => {
+    // Handle Settings navigation
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* User Info */}
+      <View style={styles.userInfoContainer}>
+        <Image
+          source={require('../assets/icon.png')}
+          style={styles.userImage}
+        />
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>{userInfo.name}</Text>
+          <Text style={styles.userRating}>Rating: {userInfo.rating}</Text>
+        </View>
+      </View>
+
+      {/* Trip History */}
+      <TouchableOpacity style={styles.menuItem} onPress={handleTripHistory}>
+        <Text style={styles.menuItemText}>Trip History</Text>
+      </TouchableOpacity>
+
+      {/* Become Driver */}
+      <TouchableOpacity style={styles.menuItem} onPress={handleBecomeDriver}>
+        <Text style={styles.menuItemText}>Become Driver</Text>
+      </TouchableOpacity>
+
+      {/* PaymentMethod */}
+      <TouchableOpacity style={styles.menuItem} onPress={handlePaymentMethod}>
+        <Text style={styles.menuItemText}>Payment Methods</Text>
+      </TouchableOpacity>
+
+      {/* About */}
+      <TouchableOpacity style={styles.menuItem} onPress={handleAbout}>
+        <Text style={styles.menuItemText}>About</Text>
+      </TouchableOpacity>
+
+      {/* Help */}
+      <TouchableOpacity style={styles.menuItem} onPress={handleHelp}>
+        <Text style={styles.menuItemText}>Help</Text>
+      </TouchableOpacity>
+
+      {/* Sign In/Out */}
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.bottomIcon} onPress={handleSignInOut}>
+          <Text style={styles.bottomIconText}>🔒</Text>
+        </TouchableOpacity>
+
+        {/* Settings */}
+        <TouchableOpacity style={styles.bottomIcon} onPress={handleSettings}>
+          <Text style={styles.bottomIconText}>⚙️</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 60
+    padding: 16,
+    backgroundColor: '#fff'
   },
-  containerVersion: {
-    bottom: device.iPhoneNotch ? 40 : 16,
-    paddingHorizontal: 38,
-    position: 'absolute',
-    width: '100%'
+  userInfoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 24
   },
-  versionText: {
-    color: colors.grey,
-    fontFamily: fonts.uberRegular,
-    fontSize: 20,
-    textAlign: 'right'
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 16
+  },
+  userInfo: {
+    flex: 1
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 4
+  },
+  userRating: {
+    fontSize: 14,
+    color: '#888'
+  },
+  menuItem: {
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
+  },
+  menuItemText: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 'auto'
+  },
+  bottomIcon: {
+    padding: 16
+  },
+  bottomIconText: {
+    fontSize: 20
   }
 });
 
-export default CustomDrawerContent;
+export default CustomwerDrawerContent;
