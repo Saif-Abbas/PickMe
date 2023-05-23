f903430057e8c53b15611db485f23d51aafbfe54
i3amber <saoab1@hotmail.com>
2023-05-23 13:36:11 +0300

    fixing
    Co-authored-by: Just4you <peekok@users.noreply.github.com>

--- src/screens/PaymentMethods.js
+++ src/screens/PaymentMethods.js
@@ -1,14 +1,37 @@
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import ModalHeader from '../components/ModalHeader';
 import { gStyle } from '../constants';
 
 const PaymentMethods = () => {
-  const test = '';
+  const paymentMethods =[
+    {id : 1, name: 'Credit Card'},
+    {id : 2, name: 'Apple Pay'},
+    {id : 3, name: 'Google Pay'},
+    {id : 4, name: 'Stc Pay'},
+    {id : 5, name: 'PayPal'},
+  ];
+  const handelPaymentMethod = (id) => {
+    console.log('Selected Payment Method: ' ,id);
+  };
+
   return (
     <View style={gStyle.container}>
       <ModalHeader text={'Payment Methods'}/>
       
+      <View style={gStyle.paymentContainer}>
+      <Text style={gStyle.paymentTitle}>Payment Methods</Text>
+      {paymentMethods.map((method) => (
+        <TouchableOpacity
+          key={method.id}
+          style={gStyle.paymentMethod}
+          onPress={() => handlePaymentMethodPress(method.id)}
+        >
+          <Text>{method.name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
     </View>
   );
 };
